f16c3a657468fe57e3ae2d10c38ba4b8ef76c858
palakonweb <palakbharadwaj1104@gmail.com>
2025-09-07 20:19:10 +0530

    Fix ESLint errors for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // other options here
 };
 
-module.exports = nextConfig;
+export default nextConfig;
